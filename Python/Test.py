0638be896aaf0da4886ba5ba597633d4a342ad0a
DHAYANITHI T <135100282+TDhayanithi@users.noreply.github.com>
2024-01-12 21:06:47 +0530

    Add files via upload

---
+++ Python/Test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### 17. Create a set of random words, and use the add() method to add a new word to the set.Print the resulting set to the console.
+
+# In[2]:
+
+
+import random
+words = {'Naruto', 'Luffy','Saitama','Goku'}
+new_words = 'Dhayanithi'
+words.add(new_words)
+print(words)
+
+
+# In[ ]:
+
+
+
+
