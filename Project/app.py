f8a679e760ad75aa1f16b81ac9e7d904ade68bc2
DHAYANITHI T <135100282+TDhayanithi@users.noreply.github.com>
2024-01-21 23:33:42 +0530

    Update app.py

--- Project/app.py
+++ Project/app.py
@@ -12,14 +12,6 @@ def text_summary(text, maxlength=None):
     result = summary(text)
     return result
 
-def extract_text_from_pdf(file_path):
-    # Open the PDF file using PyPDF2
-    with open(file_path, "rb") as f:
-        reader = PdfReader(f)
-        page = reader.pages[0]
-        text = page.extract_text()
-    return text
-
 choice = st.sidebar.selectbox("Select your choice", ["Summarize Text", "Summarize Document"])
 
 if choice == "Summarize Text":
@@ -36,22 +28,5 @@ if choice == "Summarize Text":
                 result = text_summary(input_text)
                 st.success(result)
 
-elif choice == "Summarize Document":
-    st.subheader("Summarize Document using txtai")
-    input_file = st.file_uploader("Upload your document here", type=['pdf'])
-    if input_file is not None:
-        if st.button("Summarize Document"):
-            with open("doc_file.pdf", "wb") as f:
-                f.write(input_file.getbuffer())
-            col1, col2 = st.columns([1,1])
-            with col1:
-                st.info("File uploaded successfully")
-                extracted_text = extract_text_from_pdf("doc_file.pdf")
-                st.markdown("**Extracted Text is Below:**")
-                st.info(extracted_text)
-            with col2:
-                st.markdown("**Summary Result**")
-                text = extract_text_from_pdf("doc_file.pdf")
-                doc_summary = text_summary(text)
-                st.success(doc_summary)
+   
                 
