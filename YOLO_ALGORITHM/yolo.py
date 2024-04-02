4843632579e3aa12365cef16824c29a82eedb5f8
DHAYANITHI T <135100282+TDhayanithi@users.noreply.github.com>
2024-04-02 19:40:12 +0530

    Update yolo.py

--- YOLO_ALGORITHM/yolo.py
+++ YOLO_ALGORITHM/yolo.py
@@ -1,31 +1,3 @@
-'''
-PRE-REQUISITES:
-
-Install the packages:
-
-I am using opencv 3.4.2, intall it by using the below command. Use the same opencv version
-pip install opencv-python==3.4.2
-
-We also need numpy
-pip install numpy
-
-Install argparse
-pip install argparse
-
-Download the files from the below URLs and have it inside the folder yolo-coco:
-
-https://github.com/pjreddie/darknet/blob/master/data/coco.names
-https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg
-https://pjreddie.com/media/files/yolov3.weights
-'''
-
-'''
-You can run the file by using 
-
-python yolo.py --image images/ipl.jpeg
-'''
-
-
 # importing the necessary packages
 import numpy as np
 import argparse
